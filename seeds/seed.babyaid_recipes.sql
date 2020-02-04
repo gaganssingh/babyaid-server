9684265fa801b831b33deda38bfc91586002d93d
Gagan Singh <gagansingh.dev@outlook.com>
2020-02-04 01:19:37 -0500

    Added seed data

--- seeds/seed.babyaid_recipes.sql
+++ seeds/seed.babyaid_recipes.sql
@@ -21,7 +21,7 @@ VALUES
     (
         'fourtosix', 
         'Rice Cereal',
-        ARRAY [''],
+        ARRAY ['Magnesium', 'Vitamin B6', 'Iron'],
         ARRAY ['1/4 cup brown rice', '1 cup water'],
         ARRAY ['1. Ground rice in a food processor', '2. In a saucepan, bring water to a boil and add the rice powder stirring constantly', '3. Simmer for 10 minutes, whisking constantly, mix in formula or breast milk and pureed fruits if desired. Serve warm at a safe temperature for baby.'],
         'https://infantino.com/blogs/fresh-squeezed-blog/rice-cereal-homemade-baby-food-recipe'
@@ -86,7 +86,7 @@ VALUES
     (
         'fourtosix', 
         'Mango Vanilla purée',
-        ARRAY [''],
+        ARRAY ['Vitamin A', 'Vitamin C', 'Vitamin B6', 'Magnesium', 'Calcium', 'Iron'],
         ARRAY ['16 oz frozen organic mangoes', '1/4 vanilla extract'],
         ARRAY ['1. Place the frozen mango and vanilla extract/vanilla bean into a small saucepan and heat over medium-low heat, stirring often until heated all the way through and tender roughly 3-4 minutes. Let cool slightly.', '2. Transfer the mango into a blender or food processor. If your mango mixture gets an excess of liquid while cooking, strain the mangos and reserve the mango liquid into a bowl.', '3. Blend on high for 1 minute or until the puree completely smooth.'],
         'https://babyfoode.com/blog/mango-vanilla-organic-baby-food-puree/'
@@ -193,7 +193,31 @@ VALUES
     ),
     (
         'eighttoten', 
-        'Quinoa Banana Mash',
+        'Baked apple',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Folate', 'Potassium', 'Magnesium', 'Calcium'],
+        ARRAY ['1 medium apple'],
+        ARRAY ['1. Optional (if baby has had dairy): Place a wee bit of butter on the inside of the cored apple.', '2. Place in a pan with just enough water to slightly cover apples – about an inch of water.', '3. Bake in a 400F oven for 30 minutes or until tender; be sure to check on the water level.', '4. Once baked, either cut apple into little bits and serve as a finger food or mash as directed above in the Apples/Applesauce recipe.'],
+        'https://wholesomebabyfood.momtastic.com/stage1_homemade_baby_food_recipes.htm'
+    ),
+    (
+        'eighttoten', 
+        'Homemade squash',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Vitamin B6', 'Calcium', 'Magnesium'],
+        ARRAY ['1 large winter squash'],
+        ARRAY ['1. Cut the squash in half. Scoop out the seeds.', '2. Pour a small amount of water (just enough to cover) into the bottom of a baking sheet. Place the squash halves, cut side down, on the baking sheet.', '3. Bake for 30-45 minutes in a 400 degree oven. It’s is done when the squash is tender and the tough outer skin begins to wrinkle and soften.', '4. Allow the squash halves to cool. Scoop out the squash and puree until smooth, adding breastmilk, formula, or water to achieve your desired consistency.'],
+        'https://www.babysleepsite.com/homemade-baby-food/homemade-vegetable-recipes/'
+    ),
+    (
+        'eighttoten', 
+        'Kiwi purée',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Vitamin K', 'Folate'],
+        ARRAY ['1 organic ripe kiwi', 'Cereal of choice', 'Breast milk or formula'],
+        ARRAY ['1. In a food processor puree the kiwi.', '2. Add cereal and breast milk to achieve the desired consistency.'],
+        'https://wholesomebabyfood.momtastic.com/stage2-baby-food-recipes.htm'
+    ),
+    (
+        'eighttoten', 
+        'Quinoa banana mash',
         ARRAY ['Fibre', 'Vitamin B2', 'Vitamin B6', 'Vitamin C', 'Manganese', 'Potassium', 'Magnesium', 'Folate', 'Copper'],
         ARRAY ['1/2 banana', 'Pinch cinnamon', '3 Tbsp cooked quinoa', '1 Tbsp whole milk yogurt'],
         ARRAY ['1. Mash the banana in a bowl.', '2. Add remaining ingredtents, mixing to a desired consistency.'],
@@ -201,67 +225,44 @@ VALUES
     ),
     (
         'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
+        'Effortless apples & chicken',
+        ARRAY ['Protein', 'Vitamin B6', 'Iron', 'Magnesium'],
+        ARRAY ['1/3 cup boneless chicken, chopped and cooked', '1/4 cup natural applesauce', "Cinnamon powder"],
+        ARRAY ['1. Place chicken with cooked apple in a food processor.', '2. Add cinnamon to taste.', '3. Puree or mash to a consistency suitable for baby, adding water to thin if necessary.'],
+        'https://www.thebump.com/a/baby-food-recipes-8-to-10-month-olds'
     ),
     (
         'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
+        'Pastina and bananas',
+        ARRAY ['Fibre', 'Vitamin B2', 'Vitamin B6', 'Vitamin C', 'Manganese', 'Potassium', 'Magnesium', 'Folate', 'Copper'],
+        ARRAY ['1 medium banana', '2 Tbsp wheat pastina'],
+        ARRAY ['1. Prepare pastina as directed for hot cereal. Do not add milk.', '2. Stir in banana and optionally formula or breast milk as desired.'],
+        'https://wholesomebabyfood.momtastic.com/stage2-baby-food-recipes.htm'
     ),
     (
         'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
+        'Carrot wedges',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Vitamin B6', 'Calcium', 'Magnesium'],
+        ARRAY ['1 cup carrots', 'Pinch of salt', 'Pinch of pepper powder', '1 tsp butter'],
+        ARRAY ['1. Chop carrots in wedges. Boil the water in a pan and when it comes to a boil, add the carrot pieces.', '2. Heat the butter in a pan, add the carrot pieces to it, and sauté for 2-3 minutes.', '3. Add the salt and pepper powder and mix well.', '4. Let the dish cool and serve.'],
+        'https://parenting.firstcry.com/articles/8-month-old-baby-food-ideas-chart-recipes-and-feeding-tips/'
     ),
     (
         'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
+        'Blueberry Banana Yogurt',
+        ARRAY ['Fibre', 'Vitamin B2', 'Vitamin B6', 'Vitamin C', 'Manganese', 'Potassium', 'Magnesium', 'Folate', 'Copper'],
+        ARRAY ['1 cup blueberries puree', '1 medium banana', '1 cup plain yogurt', '2 Tbsp wheat germ'],
+        ARRAY ['Place the banana, yogurt and blueberries in a blender and puree until smooth.'],
+        'https://wholesomebabyfood.momtastic.com/stage2-baby-food-recipes.htm'
     ),
+    
     (
         'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
-    ),
-    (
-        'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
-    ),
-    (
-        'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
-    ),
-    (
-        'eighttoten', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
+        'Sweet potato porridge',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Calcium', 'Vitamin B6', 'Protein'],
+        ARRAY ['2 sweet potatoes (cut, boiled)', '1 tsp grated jaggery', '1 cup milk/ almond milk', '1/4tsp cinnamon powder'],
+        ARRAY ['1. Mash boiled sweet potatoes. Take off the thread-like fiber from the boiled sweet potatoes, if any.', '2. Heat milk in a separate pan and add the mashed sweet potato and cinnamon powder. Keep stirring, continuously.', '3. Add jaggery. Switch off the stove. Allow the porridge to cool down.'],
+        'https://www.momjunction.com/articles/food-ideas-8-months-baby_0022288/'
     );
 
 INSERT INTO babyaid_recipes_tentotwelve
@@ -269,10 +270,83 @@ INSERT INTO babyaid_recipes_tentotwelve
 VALUES
     (
         'tentotwelve', 
-        '',
-        ARRAY [''],
-        ARRAY [''],
-        ARRAY [''],
-        ''
+        'Oatmeal Scramble',
+        ARRAY ['Vitamin A', 'Calcium', 'Iron', 'Fibre', 'Vitamin B6', 'Magnesium'],
+        ARRAY ['1 cup cooked oatmeal', '1/4 cup soft cooked veggies or fruits of your choice', '1 teaspoon olive oil', '2 egg yolks'],
+        ARRAY ['1. Whisk two egg yolks in a bowl and keep them aside.', '2. Peel a banana and cut it into slices.', '3. Heat olive oil in a frying pan.', '4. Add oatmeal, fruit, and egg yolk to the pan. Fry gently until the eggs are cooked.', '5. Break the oatmeal scramble into pieces and offer them to your little one.'],
+        'https://www.momjunction.com/articles/easy-yummy-oats-recipes-your-baby-will-love-to-eat_00108705/#oatmeal'
     ),
-    ('tentotwelve');
+    (
+        'tentotwelve', 
+        'Rice pulao',
+        ARRAY ['Protein', 'Magnesium', 'Vitamin B6', 'Iron'],
+        ARRAY ['2 Tbsp shredded carrot', '2 Tbsp peas', '1/2 cup cooked brown rice', '1 large egg, beaten'],
+        ARRAY ['1. Over medium heat, cook the carrots in olive oil for 2 minutes.', '2. Add the peas and 2 tablespoons water, cover, and cook until the peas are tender.', '3. Stir in the rice and cook until warmed through, then add the egg and cook.', '4. Let it cool down and serve.'],
+        'https://www.babycenter.com/0_rice-with-peas-carrots-egg_10412202.bc'
+    ),
+    (
+        'tentotwelve', 
+        'Suji Halwa',
+        ARRAY ['Protein', 'Magnesium', 'Vitamin B6', 'Calcium'],
+        ARRAY ['1/2 cup suji', '1 cup of water', '1/2 tsp ghee', '1 date, pureed'],
+        ARRAY ['1. Warm the ghee in a pan and then roast the semolina in it. Keep stirring to avoid burning.', '2. When the semolina turns aromatic, add the water and date puree.', '3. Keep the consistency of the halwa a little thin as it will thicken further while cooling.'],
+        'https://parenting.firstcry.com/articles/11-month-old-baby-food-ideas-chart-recipes-and-more/'
+    ),
+    (
+        'tentotwelve', 
+        'Pastina and bananas',
+        ARRAY ['Fibre', 'Vitamin B2', 'Vitamin B6', 'Vitamin C', 'Manganese', 'Potassium', 'Magnesium', 'Folate', 'Copper'],
+        ARRAY ['1 medium banana', '2 Tbsp wheat pastina'],
+        ARRAY ['1. Prepare pastina as directed for hot cereal. Do not add milk.', '2. Stir in banana and optionally formula or breast milk as desired.'],
+        'https://wholesomebabyfood.momtastic.com/stage2-baby-food-recipes.htm'
+    ),
+    (
+        'tentotwelve', 
+        'Sweet potato porridge',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Calcium', 'Vitamin B6', 'Protein'],
+        ARRAY ['2 sweet potatoes (cut, boiled)', '1 tsp grated jaggery', '1 cup milk/ almond milk', '1/4tsp cinnamon powder'],
+        ARRAY ['1. Mash boiled sweet potatoes. Take off the thread-like fiber from the boiled sweet potatoes, if any.', '2. Heat milk in a separate pan and add the mashed sweet potato and cinnamon powder. Keep stirring, continuously.', '3. Add jaggery. Switch off the stove. Allow the porridge to cool down.'],
+        'https://www.momjunction.com/articles/food-ideas-8-months-baby_0022288/'
+    ),    
+    (
+        'tentotwelve', 
+        'Strawberry passionfruit brulee',
+        ARRAY ['Vitamin C', 'Calcium'],
+        ARRAY ['100g strawberries', '100g unsweetened natural yogurt', '1/2 passionfruit'],
+        ARRAY ['1. Blend stratberries and yogurt into a puree.', '2. Pour mixture into bowls and add a scoop of passion fruit.', '3. Place in a glass container and store in the fridge for about 60 minutes.'],
+        'https://www.madeformums.com/baby/strawberry-and-passion-fruit-brulee/#sthash.dRGI8UHf.dpbs'
+    ),
+    (
+        'tentotwelve', 
+        'Spinach and cheese Pasta',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Calcium', 'Iron'],
+        ARRAY ['1 cup cooked pasta', '1 bunch spinach', '1 cup grated cottage cheese'],
+        ARRAY ['1. Wash the spinach well and boil it for a while. Add the cottage cheese to it and cook for a few minutes till the raw smell and taste disappear.', '2. Cool this mixture and grind it to a smooth paste with a little water.', '3. Mix this with the pasta and serve it to your baby.'],
+        'https://parenting.firstcry.com/articles/11-month-old-baby-food-ideas-chart-recipes-and-more/'
+    ),
+    (
+        'tentotwelve', 
+        'Quinoa, black beans & corn',
+        ARRAY [''],
+        ARRAY ['1/4 cup quinoa', '½ cup water', '1 tsp olive oil', '1/2 cup black beans', '1/4 corn kernels'],
+        ARRAY ['1. Cook quinoa according to the package instructions.', '2. Cook black beans and corn using the olive oil in a saucepan until tender.', '3. Mix in the cooked quinoa and serve.'],
+        'https://www.babycenter.com/0_quinoa-black-beans-corn_10403213.bc'
+    ),
+    (
+        'tentotwelve', 
+        'Baked apple',
+        ARRAY ['Vitamin A', 'Vitamin C', 'Folate', 'Potassium', 'Magnesium', 'Calcium'],
+        ARRAY ['1 medium apple'],
+        ARRAY ['1. Optional (if baby has had dairy): Place a wee bit of butter on the inside of the cored apple.', '2. Place in a pan with just enough water to slightly cover apples – about an inch of water.', '3. Bake in a 400F oven for 30 minutes or until tender; be sure to check on the water level.', '4. Once baked, either cut apple into little bits and serve as a finger food or mash as directed above in the Apples/Applesauce recipe.'],
+        'https://wholesomebabyfood.momtastic.com/stage1_homemade_baby_food_recipes.htm'
+    ),
+    (
+        'tentotwelve', 
+        'Nectarine cauliflower mash',
+        ARRAY ['Vitamin C', 'Vitamin B6', 'Iron', 'Calcium', 'Magnesium'],
+        ARRAY ['1 organic nectarine', '2 cups organic cauliflower florets'],
+        ARRAY ['1. Pour water into a pot until it is about 1 inch deep and bring to a boil over high heat.', '2. Place the nectarine and cauliflower into a steamer basket and put the basket in the pot. Make sure the water doesn’t touch the bottom of the basket.', '3. Cover the pot with a tight fitting lid and steam for about 7 minutes, until the cauliflower is very soft.', '4. Pure the nectarines and cauliflower in a food processor to a desired consistency.'],
+        'https://www.thebump.com/a/baby-food-recipes-10-to-12-months'
+    );
+
+COMMIT;
